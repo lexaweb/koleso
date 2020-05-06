a792296b44e7ba7c9021b510c621a4036bbd6179
lexaweb <alexej.stadnik@yandex.ru>
2020-05-06 20:37:15 +0300

    ...

--- src/js/main.js
+++ src/js/main.js
@@ -29,11 +29,42 @@ $(function(){
 
 $(document).click( function(e){
   if ( $(e.target).closest('.search-h').length ) {
-      // клик внутри элемента 
       return;
   }
-  // клик снаружи элемента 
   $('.search-h').removeClass('active');
   $('.icon-black').removeClass('active');
   $('.icon-white').removeClass('active');
 });
+
+const selectSingle = document.querySelector('.__select');
+const selectSingle_title = selectSingle.querySelector('.__select__title');
+const selectSingle_labels = selectSingle.querySelectorAll('.__select__label');
+
+// Toggle menu
+selectSingle_title.addEventListener('click', () => {
+  if ('active' === selectSingle.getAttribute('data-state')) {
+    selectSingle.setAttribute('data-state', '');
+  } else {
+    selectSingle.setAttribute('data-state', 'active');
+  }
+});
+
+// Close when click to option
+for (let i = 0; i < selectSingle_labels.length; i++) {
+  selectSingle_labels[i].addEventListener('click', (evt) => {
+    selectSingle_title.textContent = evt.target.textContent;
+    selectSingle.setAttribute('data-state', '');
+  });
+}
+
+(function($) {
+  $(function() {
+    
+    $('.tabs__caption').on('click', 'span:not(.active)', function() {
+      $(this)
+        .addClass('active').siblings().removeClass('active')
+        .closest('.tabs').find('.tabs__content').removeClass('active').eq($(this).index()).addClass('active');
+    });
+    
+  });
+  })(jQuery);
